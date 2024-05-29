8832ea377989d0c1d6f231bade33ee1d930a4692
Anh Ng <rean1luv.nta1510@gmail.com>
2024-05-29 22:55:07 +0700

    change to bigquery

--- dags/dags-gcp/gcp_db_scd2.py
+++ dags/dags-gcp/gcp_db_scd2.py
@@ -1,16 +1,18 @@
 from airflow.decorators import dag, task
 from datetime import datetime
 from sqlalchemy import create_engine
+from helper_gcs import GoogleCloudStorage
 
-@dag(dag_id='db_scd2',
+@dag(dag_id='gcp_db_scd2',
      start_date=datetime(2021, 10, 26),
      catchup=False,
      schedule_interval='@daily')
 def dag_etl():
     @task()
     def run_sql():
-        db_url = 'postgresql://my_user:my_password@pg-tiki:5432/dw_tiki'
-        engine = create_engine(db_url)
+        gcs_handler = GoogleCloudStorage('sch-lake-theanh')
+        db_url = 'bigquery://'
+        engine = create_engine(db_url,credentials_path=gcs_handler.keyfile_path)
         with open('dags/sql/dim_product_scd2.sql', 'r') as file:
             sql_query = file.read()
             print('sqlquery', sql_query)
